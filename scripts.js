701ee403c5e26abaed931e90aacfe28d506cf7c6
dman1991g <141966927+dman1991g@users.noreply.github.com>
2024-02-12 22:49:28 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,9 +5,9 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     var errorMessage = document.getElementById('errorMessage');
 
     // Basic hardcoded authentication
-    if (username === 'admin' && password === 'password') {
+    if (username === 'Admin' && password === 'Graphic3') {
         // Successful login, redirect to main page (in this example, just reload the page)
-        window.location.href = 'https://dman1991g.github.io/codingjourney/';
+        window.location.href = 'https://dman1991g.github.io';
     } else {
         // Display error message
         errorMessage.textContent = 'Invalid username or password';
